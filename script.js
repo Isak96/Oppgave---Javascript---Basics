ff4af1b8749e2fe774c89da10b8ced49b8b45c89
Isak96 <isak.kodehodet96@gmail.com>
2023-09-05 08:52:39 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+let DarkSoulsBosses = [
+    "Iudex Gundyr",
+    "Vordt Of The Boreal Valley",
+    "Curse-Rotted Greatwood",
+    "Crystal Sage",
+    "Deacons Of The Deep",
+    "Abyss Watchers",
+    "High Lord Wolnir",
+    "Old Demon King",
+    "Pontiff Sulyvahn",
+    "Yhorm The Giant",
+    "Aldritch Devourer Of Gods",
+    "Dancer Of The Boreal Valley",
+    "Dragonslayer Armor",
+    "Oceiros The Consumed King",
+    "Champion Gundyr",
+    "Lothric Younger Prince",
+    "Ancient Wyvern",
+    "Nameless King",
+    "Souls Of Cinder",
+    "Champion's Gravetender",
+    "Sister Friede",
+    "Halflight Spear Of The Church",
+    "Darkeater Midir",
+    "Slave Knight Gael",
+  ]
+
+  console.log(DarkSoulsBosses);
+  DarkSoulsBosses.pop();
+
+  console.log(DarkSoulsBosses);
+  DarkSoulsBosses.push("Slave Knight Gael");
+
+  console.log(DarkSoulsBosses);
+
+  DarkSoulsBosses.pop(3);
+  DarkSoulsBosses.pop(7);
+
+  console.log(DarkSoulsBosses);
+  console.log(DarkSoulsBosses.length);
+  
